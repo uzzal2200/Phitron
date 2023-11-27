4359adb6454795c838091ca30260ed152d5a21d4
uzzal2200 <129536126+uzzal2200@users.noreply.github.com>
2023-11-27 19:40:58 +0600

    Add files via upload

---
+++ Basic Data Structures/Module 03 : Prefix Sum and Binary Search/Z_Binary_Search.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,q;
+    cin>>n>>q;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+
+    while(q--)
+    {
+        int x;
+        cin>>x;
+        int l=0;
+        int r=n-1;
+        bool flag=false;
+        //binary search
+        while(l<=r)
+        {
+            int mid_index=(l+r)/2;
+            if(a[mid_index]==x)
+            {
+                flag=true;
+                break;
+            }
+            if(x>a[mid_index])
+            {
+                //dane
+                l=mid_index+1;
+            }
+            else
+            {
+                // bame jao
+            r=mid_index-1;
+            }
+        }
+       if(flag==true) cout<<"found"<<endl;
+       else cout<<"not found"<<endl;
+        
+    }
+    return 0;
+}
