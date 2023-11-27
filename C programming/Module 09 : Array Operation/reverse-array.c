ccf1a10b6a91a0bf8dac6b4f6e1808d099325ee3
uzzal2200 <129536126+uzzal2200@users.noreply.github.com>
2023-11-27 18:28:50 +0600

    Add files via upload

---
+++ C programming/Module 09 : Array Operation/reverse-array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    } 
+    int i=0,j=n-1;
+    while(i<j)
+    {
+        int tmp=ar[i];
+        ar[i]=ar[j];
+        ar[j]=tmp;
+        i++;
+        j--;
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",ar[i]);
+    }
+    printf("\n");
+  for(int i=n-1;i>0;i--)
+  {
+    printf("%d ",ar[i]);
+  }
+    return 0;
+}
